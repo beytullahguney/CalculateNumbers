e99765381ab001ef498adc0d9c51e3c9a28cf318
BGuney <beytullahguney@gmail.com>
2020-04-01 01:23:23 +0300

    Add project files.

---
+++ CalculateNumbers/CalculateHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculateNumbers
+{
+    public class CalculateHelper
+    {
+        public bool Calculate(int[] numbers, int critera)
+        {
+            bool result = false;
+            for (int j = 0; j < numbers.Length; j++)
+            {
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    if (numbers[i] == numbers[j])
+                    {
+                        continue;
+                    }
+                    if (Sum(numbers[j], numbers[i]) == critera)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return result;
+        }
+
+        private static int Sum(int first, int second)
+        {
+            return first + second;
+        }
+    }
+}
---
+++ CalculateNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CalculateNumbers
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            CalculateHelper helper = new CalculateHelper();
+            Console.WriteLine("Hello World!");
+            int[] arrayNumbers = { 1, 3, 4, 5, 7 };
+            int input = 12;
+            bool result = helper.Calculate(arrayNumbers, input);
+            Console.WriteLine(string.Format("Result {0}", result.ToString()));
+        }
+    }
+}
